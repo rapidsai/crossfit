e3527425edac6cb3f87eadc54c8d9a89856acc2d
Marc Romeyn <marcromeyn@gmail.com>
2023-02-03 14:04:54 +0100

    Fixing failing jax tests

--- tests/backends/jax_backend/test_jax_convert.py
+++ tests/backends/jax_backend/test_jax_convert.py
@@ -1,7 +1,7 @@
 import pytest
 
 import numpy as np
-import jax_backend
+import jax
 import jax.numpy as jnp
 
 from crossfit.data import convert_array
@@ -9,7 +9,7 @@ from crossfit.data import convert_array
 
 @pytest.mark.parametrize("array", [jnp.asarray, np.array])
 def test_simple_convert(array):
-    assert jax_backend.Array in convert_array.supports
-    tensor = convert_array(array([1, 2, 3]), jax_backend.Array)
+    assert jax.Array in convert_array.supports
+    tensor = convert_array(array([1, 2, 3]), jax.Array)
 
-    assert isinstance(tensor, jax_backend.Array)
+    assert isinstance(tensor, jax.Array)
