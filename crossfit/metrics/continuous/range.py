7c31e2704a152966b658f4e864fc459e203e00bf
Marc Romeyn <marcromeyn@gmail.com>
2023-02-03 16:51:01 +0100

    Adding grouped test for Dask

--- crossfit/metrics/continuous/range.py
+++ crossfit/metrics/continuous/range.py
@@ -6,14 +6,15 @@ from crossfit.metrics.base import CrossMetric, state
 class Range(CrossMetric):
     min = state(init=0, combine=np.minimum)
     max = state(init=0, combine=np.maximum)
-    
+
     def __init__(self, axis, **kwargs):
         self.axis = axis
         self.setup(**kwargs)
 
     def prepare(self, array) -> "Range":
         return Range(
-            axis=self.axis,
-            min=array.min(axis=self.axis), 
-            max=array.max(axis=self.axis)
+            axis=self.axis, min=array.min(axis=self.axis), max=array.max(axis=self.axis)
         )
+
+    def present(self):
+        return {"min": self.min, "max": self.max}
--- tests/backends/dask_backend/test_aggregate.py
+++ tests/backends/dask_backend/test_aggregate.py
@@ -11,3 +11,11 @@ def test_dask_aggregation(df, npartitions=2):
     test = aggregate(ddf, Range(axis=0), per_col=True)
 
     assert all(isinstance(x, Range) for x in test.values())
+
+
+@sample_df({"a": [1, 2] * 2000, "b": range(1000, 5000)})
+def test_dask_aggregation_grouped(df, npartitions=2):
+    ddf = dd.from_pandas(df, npartitions=npartitions)
+    test = aggregate(ddf, Range(axis=0), groupby=["a"], per_col=True)
+
+    assert all(isinstance(x, Range) for x in test.values())
