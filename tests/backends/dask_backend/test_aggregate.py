70a59516fe294255b19e7f1e21bfd97446b5697f
Marc Romeyn <marcromeyn@gmail.com>
2023-02-03 17:07:32 +0100

    Fixing failing test_dask_aggregation_grouped

--- tests/backends/dask_backend/test_aggregate.py
+++ tests/backends/dask_backend/test_aggregate.py
@@ -1,8 +1,9 @@
 import dask.dataframe as dd
 
 from crossfit.backends.dask.aggregate import aggregate
-from tests.utils import sample_df
 from crossfit.metrics.continuous.range import Range
+
+from tests.utils import is_leaf_node_instance_of, sample_df
 
 
 @sample_df({"a": [1, 2] * 2000, "b": range(1000, 5000)})
@@ -18,4 +19,4 @@ def test_dask_aggregation_grouped(df, npartitions=2):
     ddf = dd.from_pandas(df, npartitions=npartitions)
     test = aggregate(ddf, Range(axis=0), groupby=["a"], per_col=True)
 
-    assert all(isinstance(x, Range) for x in test.values())
+    assert is_leaf_node_instance_of(test, Range)
