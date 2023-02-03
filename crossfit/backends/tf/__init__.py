6eff768d634c275621e03e748867b87284b82d87
Marc Romeyn <marcromeyn@gmail.com>
2023-02-03 13:48:59 +0100

    Fix default imports to tf_backend

--- crossfit/backends/tf/__init__.py
+++ crossfit/backends/tf/__init__.py
@@ -1,5 +1,14 @@
 from crossfit.backends.tf.array import *  # noqa: F401, F403
-from crossfit.backends.tf.metrics import (  # noqa: F401, F403
-    from_tf_metric,
-    to_tf_metric,
-)
+
+try:
+    import tensorflow as tf
+
+    del tf
+
+    from crossfit.backends.tf.metrics import (  # noqa: F401, F403
+        from_tf_metric,
+        to_tf_metric,
+    )
+
+except ImportError:
+    pass
