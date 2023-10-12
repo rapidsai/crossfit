8399ded5359b6f9e2e78b528f6fb6bca0e578863
Marc Romeyn <marcromeyn@gmail.com>
2023-10-12 11:12:42 +0200

    Change default imports

--- crossfit/op/__init__.py
+++ crossfit/op/__init__.py
@@ -1,22 +1,31 @@
 from crossfit.op.base import Op
 from crossfit.op.combinators import Sequential
-from crossfit.op.vector_search import CuMLANNSearch, CuMLExactSearch, RaftExactSearch
 from crossfit.op.tokenize import Tokenizer
 
 
 __all__ = [
-    "CuMLANNSearch",
-    "CuMLExactSearch",
-    "CuMLANNSearch",
-    "RaftExactSearch",
     "Op",
     "Sequential",
-    "Tokenizer",
 ]
 
 try:
     from crossfit.backend.torch.op.embed import Embedder
     
     __all__.append("Embedder")
+except ImportError:
+    pass
+
+
+try:
+    from crossfit.backend.torch.op.embed import Tokenizer
+    
+    __all__.append("Tokenizer")
+except ImportError:
+    pass
+
+
+try:
+    from crossfit.op.vector_search import CuMLANNSearch, CuMLExactSearch, RaftExactSearch
+    __all__.extend(["CuMLANNSearch", "CuMLExactSearch", "RaftExactSearch"])
 except ImportError:
     pass
