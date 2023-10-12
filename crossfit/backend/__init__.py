bf710d71c7fe56b54492c4f9980d6a1d4eda022a
Marc Romeyn <marcromeyn@gmail.com>
2023-10-12 10:53:21 +0200

    Remove some default backend imports

--- crossfit/backend/__init__.py
+++ crossfit/backend/__init__.py
@@ -1,12 +1,25 @@
-from crossfit.backend.cudf.array import *
-from crossfit.backend.cudf.dataframe import *
-from crossfit.backend.cupy.array import *
-from crossfit.backend.cupy.sparse import *
 from crossfit.backend.numpy.sparse import *
 from crossfit.backend.dask.dataframe import *
 from crossfit.backend.pandas.array import *
 from crossfit.backend.pandas.dataframe import *
-from crossfit.backend.torch.array import *
+
+
+try:
+    from crossfit.backend.cudf.array import *
+    from crossfit.backend.cudf.dataframe import *
+except ImportError:
+    pass
+
+try:
+    from crossfit.backend.cupy.array import *
+    from crossfit.backend.cupy.sparse import *
+except ImportError:
+    pass
+
+try:
+    from crossfit.backend.torch.array import *
+except ImportError:
+    pass
 
 # from crossfit.backend.tf.array import *
 # from crossfit.backend.jax.array import *
