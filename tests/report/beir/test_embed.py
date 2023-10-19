a0b1477a5f86e9b018b9bbfc21611f0077a55c79
edknv <edwardk@nvidia.com>
2023-10-19 13:46:59 -0700

    separate singlegpu and multigpu workflows

--- tests/report/beir/test_embed.py
+++ tests/report/beir/test_embed.py
@@ -16,10 +16,35 @@ DATASETS.discard("webis-touche2020")
 DATASETS.discard("trec-covid")
 
 
-@pytest.mark.multigpu
-def test_embed_basic(
+@pytest.mark.singlegpu
+def test_embed_multi_gpu(
     model_name="all-MiniLM-L6-v2",
     rmm_pool_size="12GB",
+    k=10,
+):
+    model = cf.SentenceTransformerModel(model_name)
+    vector_search = cf.TorchExactSearch(k=k)
+    with cf.Distributed(rmm_pool_size=rmm_pool_size, n_workers=1):
+        for dataset in DATASETS:
+            embeds = cf.embed(
+                dataset,
+                model,
+                vector_search=vector_search,
+                overwrite=True,
+                tiny_sample=True,
+            )
+            embeds = embeds.predictions.ddf().compute().to_pandas()
+
+        assert set(embeds.columns) == set(
+            ["corpus-index", "score", "query-id", "query-index"]
+        )
+        assert embeds["query-index"].nunique() == embeds["query-id"].nunique()
+
+
+@pytest.mark.multigpu
+def test_embed_multi_gpu(
+    model_name="all-MiniLM-L6-v2",
+    rmm_pool_size="24GB",
     k=10,
 ):
     model = cf.SentenceTransformerModel(model_name)
--- tests/report/beir/test_report.py
+++ tests/report/beir/test_report.py
@@ -19,6 +19,37 @@ DATASETS.discard("dbpedia-entity")
 DATASETS.discard("trec-covid")
 
 
+@pytest.mark.singlegpu
+def test_beir_report(model_name="all-MiniLM-L6-v2", k=10):
+    model = cf.SentenceTransformerModel(model_name)
+    vector_search = cf.TorchExactSearch(k=k)
+
+    with cf.Distributed(rmm_pool_size="12GB", n_workers=1):
+        for dataset in DATASETS:
+            report = cf.beir_report(
+                dataset,
+                model,
+                vector_search=vector_search,
+                overwrite=True,
+                tiny_sample=True,
+            )
+
+        expected_columns = [
+            f"{metric}@{k}"
+            for metric in ["NDCG", "Recall", "Precision"]
+            for k in [1, 3, 5, 10]
+        ]
+        expected_indices = [
+            ("split", "test"),
+            ("split", "train"),
+            ("split", "val"),
+        ]
+        assert sorted(report.result_df.columns.tolist()) == sorted(expected_columns)
+        assert ("split", "test") in report.result_df.index.values.tolist()
+        for col in expected_columns:
+            assert report.result_df.loc[("split", "test"), col].item() > 0.0
+
+
 @pytest.mark.multigpu
 def test_beir_report(model_name="all-MiniLM-L6-v2", k=10):
     model = cf.SentenceTransformerModel(model_name)
