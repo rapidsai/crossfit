45f4239e4232ed31f000c3cb96dd6fc738c88103
Oliver Holworthy <1216955+oliverholworthy@users.noreply.github.com>
2023-10-20 19:33:33 +0100

    Use greedy approach to finding batch sizes in SortedSeqLoader

--- crossfit/backend/torch/hf/model.py
+++ crossfit/backend/torch/hf/model.py
@@ -104,5 +104,6 @@ class SentenceTransformerModel(HFModel):
 
         return SentenceTransformer(self.path_or_name, device="cuda").to(device)
 
+    @lru_cache(maxsize=1)
     def load_cfg(self):
         return AutoConfig.from_pretrained("sentence-transformers/" + self.path_or_name)
