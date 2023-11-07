64a5b3ad8e9aa87ddc722f65da3c117d00af98cb
edknv <edwardk@nvidia.com>
2023-11-07 10:28:56 -0800

    remove slow marker

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,7 +2,6 @@ import pytest
 
 
 def pytest_collection_modifyitems(config, items):
-    skip_slow = pytest.mark.skip(reason="need --runslow option to run")
     for item in items:
         path = item.location[0]
         if "/tf_backend/" in path:
@@ -15,5 +14,3 @@ def pytest_collection_modifyitems(config, items):
             item.add_marker(pytest.mark.tensorflow)
             item.add_marker(pytest.mark.pytorch)
             item.add_marker(pytest.mark.jax)
-        if "slow" in item.keywords:
-            item.add_marker(skip_slow)
--- tests/report/beir/test_report.py
+++ tests/report/beir/test_report.py
@@ -82,7 +82,6 @@ def test_no_invalid_scores(
     assert not np.isinf(ndcg).any()
 
 
-@pytest.mark.slow
 @pytest.mark.benchmark(
     warmup_iterations=1,
 )
