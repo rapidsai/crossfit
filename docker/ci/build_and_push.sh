4bc27f51524c61f73a42a1d8aed4f10f3ab19e70
edknv <edwardk@nvidia.com>
2023-11-09 21:00:40 -0800

    run ci in custom container with model and data

---
+++ docker/ci/build_and_push.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Usage:
+#  $ NVCR_PAT=YOUR_TOKEN ./docker/ci/build_and_push.sh
+
+set -e
+
+IMAGE_NAME=nvcr.io/nvidian/crossfit-ci
+IMAGE_TAG=23.09
+
+docker build -t ${IMAGE_NAME}:${IMAGE_TAG} -f docker/ci/Dockerfile .
+
+if [ -z ${NVCR_PAT+x} ]
+then
+  echo "This script assumes your nvcr personal access token is stored in "
+  echo "a variable named NVCR_PAT, e.g., export NVCR_PAT=YOUR_TOKEN. "
+  exit 1
+fi
+
+echo ${NVCR_PAT} | docker login nvcr.io -u '$oauthtoken' --password-stdin
+
+docker push ${IMAGE_NAME}:${IMAGE_TAG}
