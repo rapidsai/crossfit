c22c64e7e1e9cec3d292c0f9e7008503f005cec3
edknv <edwardk@nvidia.com>
2023-12-17 22:34:46 -0800

    test example scripts in ci

---
+++ tests/examples/test_scripts.py
@@ -0,0 +1,33 @@
+import os
+import runpy
+import shutil
+import sys
+import tempfile
+
+import pytest
+
+examples_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), "..", "..", "examples")
+
+
+@pytest.mark.singlegpu
+@pytest.mark.parametrize(
+    "script",
+    [
+        "beir_report.py",
+    ],
+)
+def test_script_execution(script):
+    path = os.path.join(examples_dir, script)
+    orig_sys_argv = sys.argv
+
+    with tempfile.TemporaryDirectory() as tmpdir:
+        tmp_path = os.path.join(tmpdir, script)
+        shutil.copy2(path, tmp_path)
+        # argv[0] will be replaced by runpy
+        sys.argv = ["", "--dataset", "fiqa"]
+        runpy.run_path(
+            tmp_path,
+            run_name="__main__",
+        )
+
+    sys.argv = orig_sys_argv
