fa6502a7087e043733a03253350412155130a96c
edknv <edwardk@nvidia.com>
2024-01-16 10:25:12 -0800

    lint

--- tests/examples/test_scripts.py
+++ tests/examples/test_scripts.py
@@ -7,9 +7,9 @@ import runpy  # noqa: E402
 import shutil  # noqa: E402
 import sys  # noqa: E402
 import tempfile  # noqa: E402
-from uuid import uuid4  # noqa: E402
+# from uuid import uuid4  # noqa: E402
 
-from crossfit.dataset.load import load_dataset  # noqa: E402
+# from crossfit.dataset.load import load_dataset  # noqa: E402
 
 examples_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), "..", "..", "examples")
 
@@ -50,17 +50,17 @@ def test_beir_report():
 # def test_custom_pytorch_model():
 #     path = os.path.join(examples_dir, "custom_pytorch_model.py")
 #     orig_sys_argv = sys.argv
-# 
+#
 #     with tempfile.TemporaryDirectory() as tmpdir:
 #         tmp_path = os.path.join(tmpdir, "custom_pytorch_model.py")
 #         shutil.copy2(path, tmp_path)
-# 
+#
 #         dataset = load_dataset("beir/fiqa")
 #         dataset_path = os.path.join(tmpdir, f"{str(uuid4())}.parquet")
 #         dataset.item.ddf().sample(frac=0.01).to_parquet(dataset_path)
-# 
+#
 #         output_path = os.path.join(tmpdir, f"{str(uuid4())}.parquet")
-# 
+#
 #         # argv[0] will be replaced by runpy
 #         sys.argv = [
 #             "",
@@ -77,9 +77,9 @@ def test_beir_report():
 #             tmp_path,
 #             run_name="__main__",
 #         )
-# 
+#
 #         df = cudf.read_parquet(output_path)
 #         labels = ["foo", "bar", "baz"]
 #         assert all(x in labels for x in df["labels"].unique().to_arrow().to_pylist())
-# 
+#
 #     sys.argv = orig_sys_argv
