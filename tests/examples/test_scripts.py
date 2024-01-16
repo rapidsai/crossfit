663dc1805bf1848f27105b014f31f14869df14ac
edknv <edwardk@nvidia.com>
2024-01-16 09:43:09 -0800

    update custom pytorch example

--- tests/examples/test_scripts.py
+++ tests/examples/test_scripts.py
@@ -7,6 +7,9 @@ import runpy  # noqa: E402
 import shutil  # noqa: E402
 import sys  # noqa: E402
 import tempfile  # noqa: E402
+from uuid import uuid4  # noqa: E402
+
+from crossfit.dataset.load import load_dataset  # noqa: E402
 
 examples_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), "..", "..", "examples")
 
@@ -43,40 +46,40 @@ def test_beir_report():
 
 
 # Works locally (A6000) but does work in CI (P100)
-# @pytest.mark.singlegpu
-# def test_custom_pytorch_model():
-#     path = os.path.join(examples_dir, "custom_pytorch_model.py")
-#     orig_sys_argv = sys.argv
-#
-#     with tempfile.TemporaryDirectory() as tmpdir:
-#         tmp_path = os.path.join(tmpdir, "custom_pytorch_model.py")
-#         shutil.copy2(path, tmp_path)
-#
-#         dataset = load_dataset("beir/fiqa")
-#         dataset_path = os.path.join(tmpdir, f"{str(uuid4())}.parquet")
-#         dataset.item.ddf().sample(frac=0.01).to_parquet(dataset_path)
-#
-#         output_path = os.path.join(tmpdir, f"{str(uuid4())}.parquet")
-#
-#         # argv[0] will be replaced by runpy
-#         sys.argv = [
-#             "",
-#             f"{dataset_path}",
-#             f"{output_path}",
-#             "--pool-size",
-#             "4GB",
-#             "--batch-size",
-#             "8",
-#             "--partitions",
-#             "20",
-#         ]
-#         runpy.run_path(
-#             tmp_path,
-#             run_name="__main__",
-#         )
-#
-#         df = cudf.read_parquet(output_path)
-#         labels = ["foo", "bar", "baz"]
-#         assert all(x in labels for x in df["labels"].unique().to_arrow().to_pylist())
-#
-#     sys.argv = orig_sys_argv
+@pytest.mark.singlegpu
+def test_custom_pytorch_model():
+    path = os.path.join(examples_dir, "custom_pytorch_model.py")
+    orig_sys_argv = sys.argv
+
+    with tempfile.TemporaryDirectory() as tmpdir:
+        tmp_path = os.path.join(tmpdir, "custom_pytorch_model.py")
+        shutil.copy2(path, tmp_path)
+
+        dataset = load_dataset("beir/fiqa")
+        dataset_path = os.path.join(tmpdir, f"{str(uuid4())}.parquet")
+        dataset.item.ddf().sample(frac=0.01).to_parquet(dataset_path)
+
+        output_path = os.path.join(tmpdir, f"{str(uuid4())}.parquet")
+
+        # argv[0] will be replaced by runpy
+        sys.argv = [
+            "",
+            f"{dataset_path}",
+            f"{output_path}",
+            "--pool-size",
+            "4GB",
+            "--batch-size",
+            "8",
+            "--partitions",
+            "20",
+        ]
+        runpy.run_path(
+            tmp_path,
+            run_name="__main__",
+        )
+
+        df = cudf.read_parquet(output_path)
+        labels = ["foo", "bar", "baz"]
+        assert all(x in labels for x in df["labels"].unique().to_arrow().to_pylist())
+
+    sys.argv = orig_sys_argv
