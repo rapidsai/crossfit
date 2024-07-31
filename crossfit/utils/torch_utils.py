91dd34df24a3dc82e127b63188c6f7621a16b6b0
Vibhu Jawa <vibhujawa@gmail.com>
2024-07-31 00:58:54 -0700

    Add docstrings
    Signed-off-by: Vibhu Jawa <vibhujawa@gmail.com>

--- crossfit/utils/torch_utils.py
+++ crossfit/utils/torch_utils.py
@@ -1,8 +1,26 @@
+from typing import List, Union
+
 import torch
 import torch.nn.functional as F
 
 
-def pad_tensors(tensor_list, pad_token_id=0):
+def pad_tensors(
+    tensor_list: List[torch.Tensor], pad_token_id: Union[int, float] = 0
+) -> List[torch.Tensor]:
+    """
+    Pad a list of tensors to the same shape.
+
+    This function takes a list of tensors with potentially different shapes and pads them
+    to match the largest dimensions across all tensors in the list. The padding is applied
+    to the end of each dimension.
+
+    Args:
+        tensor_list (List[torch.Tensor]): A list of tensors to be padded.
+        pad_token_id (Union[int, float], optional): The value to use for padding. Defaults to 0.
+
+    Returns:
+        List[torch.Tensor]: A list of padded tensors, all with the same shape.
+    """
     # Find the maximum dimensions
     max_dims = [
         max(tensor.shape[i] for tensor in tensor_list) for i in range(len(tensor_list[0].shape))
@@ -22,7 +40,23 @@ def pad_tensors(tensor_list, pad_token_id=0):
     return padded_tensors
 
 
-def concat_and_pad_tensors(all_outputs_ls, pad_token_id=0):
+def concat_and_pad_tensors(
+    all_outputs_ls: List[torch.Tensor], pad_token_id: Union[int, float] = 0
+) -> torch.Tensor:
+    """
+    Concatenate a list of tensors after padding them to the same shape.
+
+    This function first pads all input tensors to the same shape using the `pad_tensors`
+    function, then concatenates them along the first dimension.
+
+    Args:
+        all_outputs_ls (List[torch.Tensor]): A list of tensors to be padded and concatenated.
+        pad_token_id (Union[int, float], optional): The value to use for padding. Defaults to 0.
+
+    Returns:
+        torch.Tensor: A single tensor resulting from the concatenation of all padded input tensors.
+
+    """
     # Ensure all tensors are on the same device
     device = all_outputs_ls[0].device
     all_outputs_ls = [tensor.to(device) for tensor in all_outputs_ls]
