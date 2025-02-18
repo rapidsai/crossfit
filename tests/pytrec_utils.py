654e1dcdcac2ecd21221202fe59b3adbf656915d
Sarah Yurick <53962159+sarahyurick@users.noreply.github.com>
2025-02-18 10:33:44 -0800

    Adjust `keep_cols` logic (#109)
    * Adjust `keep_cols` logic
    
    Signed-off-by: Sarah Yurick <sarahyurick@gmail.com>
    
    * flake8 lint
    
    Signed-off-by: Sarah Yurick <sarahyurick@gmail.com>
    
    ---------
    
    Signed-off-by: Sarah Yurick <sarahyurick@gmail.com>

--- tests/pytrec_utils.py
+++ tests/pytrec_utils.py
@@ -24,13 +24,13 @@ def create_qrel(relevance_scores, ids=None):
 
     qrel = {}
     for i, query_scores in enumerate(relevance_scores):
-        query_id = ids[i] if ids is not None else f"q{i+1}"
+        query_id = ids[i] if ids is not None else f"q{i + 1}"
         qrel[query_id] = {}
         for j, score in enumerate(query_scores):
             _score = int(score.item())
 
             if _score > 0:
-                doc_id = f"d{j+1}"
+                doc_id = f"d{j + 1}"
                 qrel[query_id][doc_id] = int(score.item())
 
     return qrel
@@ -41,10 +41,10 @@ def create_run(predicted_scores, ids=None):
 
     run = {}
     for i, query_scores in enumerate(predicted_scores):
-        query_id = ids[i] if ids is not None else f"q{i+1}"
+        query_id = ids[i] if ids is not None else f"q{i + 1}"
         run[query_id] = {}
         for j, score in enumerate(query_scores):
-            doc_id = f"d{j+1}"
+            doc_id = f"d{j + 1}"
             run[query_id][doc_id] = float(score.item())
 
     return run
@@ -60,6 +60,6 @@ def create_results(metric_arrays):
         for k, v in metric_arrays.items():
             q_out[k] = float(v[i])
 
-        outputs[f"q{i+1}"] = q_out
+        outputs[f"q{i + 1}"] = q_out
 
     return outputs
