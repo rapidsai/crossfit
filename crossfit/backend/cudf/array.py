12d90f5682c8938f2f481189f00033aae859b846
Sarah Yurick <53962159+sarahyurick@users.noreply.github.com>
2025-04-23 12:04:52 -0700

    Update array.py

--- crossfit/backend/cudf/array.py
+++ crossfit/backend/cudf/array.py
@@ -14,8 +14,6 @@
 
 import logging
 
-import numpy as np
-
 from crossfit.data.array import conversion
 from crossfit.data.array.dispatch import ArrayBackend, np_backend_dispatch
 
@@ -46,14 +44,8 @@ def register_cudf_to_dlpack():
             if not input_array.list.len().min() == input_array.list.len().max():
                 raise NotImplementedError("Cannot convert list column with variable length")
 
-            try:
-                dim = input_array.list.len().iloc[0]
-                return input_array.list.leaves.values.reshape(-1, dim).toDlpack()
-            except Exception:
-                import torch
-
-                # Less memory efficient fallback
-                return torch.tensor(np.stack(input_array.to_arrow().to_pylist()), device="cuda")
+            dim = input_array.list.len().iloc[0]
+            return input_array.list.leaves.values.reshape(-1, dim).toDlpack()
 
         return input_array.to_dlpack()
 
